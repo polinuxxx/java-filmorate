da571ecea530f16a4993e7a4fe7a2d80a1d375bd
PavelIgK <pavel.kistenyov@yandex.ru>
2023-11-30 15:53:25 +0300

    refactor:checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/dto/request/FilmUpdateRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/request/FilmUpdateRequest.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
