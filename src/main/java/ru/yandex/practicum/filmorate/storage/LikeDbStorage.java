c52738a4988de4d5c8eaa6f2dbd25afa29ccad9a
polinuxxx <polinakazichkina@yandex.ru>
2023-11-30 21:09:38 +0300

    fix: postman tests in develop

--- src/main/java/ru/yandex/practicum/filmorate/storage/LikeDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/LikeDbStorage.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import ru.yandex.practicum.filmorate.exception.EntityAlreadyExistsException;
-import ru.yandex.practicum.filmorate.exception.EntityNotFoundException;
 
 /**
  * DAO для лайков фильмов.
@@ -18,31 +16,12 @@ public class LikeDbStorage {
 
     @Transactional
     public void addLikeToFilm(Long filmId, Long userId) {
-        if (exists(filmId, userId)) {
-            throw new EntityAlreadyExistsException(String.format("Фильму с id = %d уже добавлен лайк от пользователя " +
-                    "с id = %d", filmId, userId));
-        }
-
-        jdbcTemplate.update("insert into likes (film_id, user_id) values (?, ?)",
+        jdbcTemplate.update("merge into likes (film_id, user_id) values (?, ?)",
                 filmId, userId);
     }
 
     @Transactional
     public void deleteLikeFromFilm(Long filmId, Long userId) {
-        if (!exists(filmId, userId)) {
-            throw new EntityNotFoundException(String.format("У фильма с id = %d отсутствует лайк от пользователя" +
-                    " с id = %d", filmId, userId));
-        }
-
         jdbcTemplate.update("delete from likes where film_id = ? and user_id = ?", filmId, userId);
-    }
-
-    @Transactional(readOnly = true)
-    public boolean exists(Long filmId, Long userId) {
-        String sql = "select case when count(film_id) > 0 then true else false end " +
-                "from likes where film_id = ? and user_id = ?";
-        Boolean exists = jdbcTemplate.queryForObject(sql, Boolean.class, filmId, userId);
-
-        return exists != null && exists;
     }
 }
